6dcde5ecd9069a20d79d8643f028d8144db52866
PeterWalchhofer <peter.walchhofer@liwest.at>
2023-04-05 21:04:57 +0200

    add button link to dashboard

--- pages/h/[host]/index.tsx
+++ pages/h/[host]/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import { useRouter } from "next/router";
 import { Indices, useAmcatIndices, useMiddlecatContext } from "../../../amcat4react";
 import CreateIndex from "../../../amcat4react/components/Indices/CreateIndex";
@@ -14,12 +15,26 @@ export default function IndexSelection() {
     if (!user) return;
     router.push(link_query(user.resource, index));
   }
+
   if (!user || !indices.data) return;
+
   return (
     <>
       <div>{!is_writer ? null : <CreateIndex user={user} onCreate={() => indices.refetch()} />}</div>
+      <div style={{marginTop: "15px"}}>
+      <Button
+              variant="outlined"
+              style={{
+                fontSize: "1.5rem",
+                color: "green",
+                borderColor: "green",
+              }}
+              onClick={() => router.push("/dashboard")}
+            >
+              Dashboard
+            </Button>
+            </div>
       <h2>{indices.data.length > 0 ? "Select index" : "No indices available"}</h2>
-
       <Indices user={user} onSelect={onSelectIndex} />
     </>
   );
